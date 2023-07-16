6baa2e870c9ccab930a1ba2f7eec3f91cccb36c8
Brendan-funk <brendanzfunk@gmail.com>
2023-07-15 21:40:21 -0700

    continued working on Teams component and minor styling

--- kelowna-gas/src/components/Teams.js
+++ kelowna-gas/src/components/Teams.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import owLogo from '../photos/overwatch.png';
+import val from '../photos/val.png';
 import './Teams.css';
 
 
@@ -10,6 +11,11 @@ export default function Teams() {
             <div className='GameHeader'>
                 <img src={owLogo} className='OwLogo' alt='logo'/>
                 <h2 className='GameName'>Overwatch 2</h2>
+            </div>
+            <p className='GameWriteUp'>Endeavor bachelor but add eat pleasure doubtful sociable. Age forming covered you entered the examine. Blessing scarcely confined her contempt wondered shy. Dashwoods contented sportsmen at up no convinced cordially affection. Am so continued resembled frankness disposing engrossed dashwoods. Earnest greater on no observe fortune norland. Hunted mrs ham wishes stairs. Continued he as so breakfast shameless. All men drew its post knew. Of talking of calling however civilly wishing resolve. </p>
+            <div className='GameHeader'>
+                <img src={val} className='Val' alt='logo'/>
+                <h2 className='GameName'>Valorant</h2>
             </div>
             <p className='GameWriteUp'>Endeavor bachelor but add eat pleasure doubtful sociable. Age forming covered you entered the examine. Blessing scarcely confined her contempt wondered shy. Dashwoods contented sportsmen at up no convinced cordially affection. Am so continued resembled frankness disposing engrossed dashwoods. Earnest greater on no observe fortune norland. Hunted mrs ham wishes stairs. Continued he as so breakfast shameless. All men drew its post knew. Of talking of calling however civilly wishing resolve. </p>
         </div>
